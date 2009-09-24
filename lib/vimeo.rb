e6d161d0efcc203f9564fa83eeff33c53bed807d
matthooks <matthooks@gmail.com>
2009-09-23 21:12:40 -0500

    no longer need to require cgi

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -1,12 +1,10 @@
 require 'rubygems'
 require 'httparty'
 require 'digest/md5'
-require 'cgi'
 
 $:.unshift(File.dirname(__FILE__))
 require 'vimeo/simple'
 require 'vimeo/advanced'
 
 module Vimeo
-  
 end
