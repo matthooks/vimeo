c8a9f7cbe54de68a552bfb1a6baf6b4f6598282c
Pavel <pavel.evst@gmail.com>
2011-09-16 03:57:54 +0800

    clean dependences and gemfile

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -1,11 +1,7 @@
 require 'rubygems'
-require 'httparty'
 require 'digest/md5'
 
 require 'net/http/post/multipart'
-
-#require 'active_support'
-#require 'active_support/core_ext'
 
 $:.unshift(File.dirname(__FILE__))
 require 'vimeo/simple'
--- lib/vimeo/simple/base.rb
+++ lib/vimeo/simple/base.rb
@@ -1,10 +1,14 @@
+require "open-uri"
+
 module Vimeo
   module Simple
     
-    class Base      
-      include HTTParty
-      base_uri 'vimeo.com/api/v2'
+    class Base
+      BASE_URL = 'vimeo.com/api/v2'
+      
+      def self.get(path)
+        Vimeo.parse_json(open("http://#{BASE_URL}#{path}").read)
+      end
     end # Base
-
   end # Simple
 end # Vimeo
