dbde05c59ba10fb13451bc69434a10a53fee9c16
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-10-10 16:01:26 -0300

    Add the posibility to use a vimeo URL to get video information

--- test/vimeo/simple/video_test.rb
+++ test/vimeo/simple/video_test.rb
@@ -6,6 +6,7 @@ class VideoTest < Test::Unit::TestCase
     
     setup do
       @video_id = "411684"
+      @vimeo_url = "http://vimeo.com/#{@video_id}"
     end
     
     context "making api calls" do
@@ -13,6 +14,13 @@ class VideoTest < Test::Unit::TestCase
       should "be able to get information about a video" do
         stub_get("/video/#{@video_id}.json", "simple/video/info.json")
         info = Vimeo::Simple::Video.info(@video_id).first
+        
+        assert_equal "411684", info["id"]
+      end
+
+      should "be able to use a vimeo url to get information about a video" do
+        stub_get("/video/#{@video_id}.json", "simple/video/info.json")
+        info = Vimeo::Simple::Video.info(@vimeo_url).first
         
         assert_equal "411684", info["id"]
       end
