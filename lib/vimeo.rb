b81b329b0c75c2a5b114477116e68b96efe95443
Colby Swandale <colby@taplaboratories.com.au>
2015-01-12 21:28:00 +1100

    added base lib structure

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -1,12 +1,1 @@
-require 'rubygems'
-require 'httparty'
-require 'digest/md5'
-
-require 'net/http/post/multipart'
-
-$:.unshift(File.dirname(__FILE__))
-require 'vimeo/simple'
-require 'vimeo/advanced'
-
-module Vimeo
-end
+require "vimeo/version"
