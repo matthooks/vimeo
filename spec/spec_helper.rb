fb1425d9e6d986c4cc8e3ce6dbaae8f3e43acf6d
Colby Swandale <colby@taplaboratories.com.au>
2015-01-21 21:09:55 +1100

    added vimeo_access_token global variable to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,12 @@ require "support/vcr"
 # lib/
 require "vimeo"
 
+# This is the Vimeo API access token used to authenticate requests their the API.
+# Most requests should already be recorded with VCR so it shouldn't be
+# nessesary to change this value. If there is a request not recorded in
+# VCR then edit this to the access token given by Vimeo.
+$vimeo_access_token = 'ah'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
