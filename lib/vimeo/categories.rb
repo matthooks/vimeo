6c97d5d8e8d3c05e27d8d8f7f997271fdd8e220b
Colby Swandale <colby@taplaboratories.com.au>
2015-01-21 22:54:53 +1100

    removed .json extension from categories endpoint

--- lib/vimeo/categories.rb
+++ lib/vimeo/categories.rb
@@ -3,11 +3,11 @@ module Vimeo
     include Vimeo::Helpers
 
     def categories options = {}
-      perform_get_with_object("/categories.json", options, Vimeo::Entities::Category)
+      perform_get_with_object("/categories", options, Vimeo::Entities::Category)
     end
 
     def category id, options = {}
-      perform_get_with_object("/categories/#{id}.json", options, Vimeo::Entities::Category)
+      perform_get_with_object("/categories/#{id}", options, Vimeo::Entities::Category)
     end
   end
 end
