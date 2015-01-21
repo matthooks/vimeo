0270a52b28e6d4ab63e0d26654b585df9c9b9b1e
Colby Swandale <colby@taplaboratories.com.au>
2015-01-21 22:42:10 +1100

    autobuild cassets for vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,3 +2,10 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
   config.hook_into :webmock
 end
+
+RSpec.configure do |c|
+  c.around(:each, :vcr) do |example|
+    name = example.metadata[:full_description]
+    VCR.use_cassette(name) { example.call }
+  end
+end
