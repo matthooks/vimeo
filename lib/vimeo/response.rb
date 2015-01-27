7fc9fc976f32b4362c1cf0a805da23a6327aec39
Colby Swandale <colby@taplaboratories.com.au>
2015-01-27 22:29:38 +1100

    parse json with JSON instead of symbolize_keys

--- lib/vimeo/response.rb
+++ lib/vimeo/response.rb
@@ -1,14 +1,16 @@
+require "json"
+
 module Vimeo
   class Response
     def initialize response
       @headers  = response.headers
       @status   = response.status
-      @body     = symbolize_keys! response.body
+      @body     = response.body
     end
 
     def parse_response_or_fail
       error = error(@headers, @status, @response)
-      @body
+      body  = parse(@body)
     end
 
     protected
@@ -16,17 +18,8 @@ module Vimeo
     def error(headers, status, response)
     end
 
-    def symbolize_keys! object
-      if object.is_a?(Array)
-        object.each_with_index do |val, index|
-          object[index] = symbolize_keys!(val)
-        end
-      elsif object.is_a?(Hash)
-        object.keys.each do |key|
-        object[key.to_sym] = symbolize_keys!(object.delete(key))
-        end
-      end
-      object
+    def parse response
+      JSON.parse(response, symbolize_names: true)
     end
   end
 end
