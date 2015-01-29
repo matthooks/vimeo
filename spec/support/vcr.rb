89155fbdceaea990b8270a8fb8cc563cfaec5ef9
Colby Swandale <colby@taplaboratories.com.au>
2015-01-29 21:01:49 +1100

    mark the Authroization header as sensitive in vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,9 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data '<ACCESS_TOKEN>' do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
 end
 
 RSpec.configure do |c|
