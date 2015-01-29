2e414ec93f74f57b73d1f316c1e32abd6151a6eb
Colby Swandale <colby@taplaboratories.com.au>
2015-01-29 22:07:06 +1100

    categories are now passing all tests

---
+++ spec/vimeo/categories_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe Vimeo::Categories do
+
+  let(:client) do
+    Vimeo::Client.new {|c| c.access_token = $vimeo_access_token }
+  end
+
+  describe '.categories', :vcr do
+    subject { client.categories }
+
+    it { is_expected.to be_a_kind_of(Vimeo::Collection) }
+
+    it 'has a collection of categories' do
+      expect(subject.first).to be_a_kind_of(Vimeo::Entities::Category)
+    end
+  end
+
+  describe '.category', :vcr do
+    subject { client.category 1 }
+
+    it { is_expected.to be_a_kind_of(Vimeo::Entities::Category) }
+  end
+end
