549cad957433a8b07532cc154a84da9a52c0d004
Colby Swandale <colby@taplaboratories.com.au>
2015-02-03 22:01:38 +1100

    added implementation for Category entity methods

--- lib/vimeo/entities/category.rb
+++ lib/vimeo/entities/category.rb
@@ -1,22 +1,31 @@
 module Vimeo
   module Entities
     class Category < Vimeo::Base
+      include Vimeo::Helpers
 
       ##
       # Get a list of channels related to this category
-      def channels; end
+      def channels options = {}
+        perform_get_with_object("/categories/#{name}/channels", options, Vimeo::Entities::Channel)
+      end
 
       ##
       # Get a list of groups related to this category
-      def groups; end
+      def groups options = {}
+        perform_get_with_object("/categories/#{name}/groups", options, Vimeo::Entities::Group)
+      end
 
       ##
       # Get a list of users related to this category
-      def users; end
+      def users options = {}
+        perform_get_with_object("/categories/#{name}/users", options, Vimeo::Entities::User)
+      end
 
       ##
       # Get a list of videos related to this category
-      def videos; end
+      def videos options = {}
+        perform_get_with_object("/categories/#{name}/videos", options, Vimeo::Entities::Video)
+      end
     end
   end
 end
