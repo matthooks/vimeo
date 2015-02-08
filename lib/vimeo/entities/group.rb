b08fdb016e11cd75ef064e076f98e8467bb8365b
Colby Swandale <colby@taplaboratories.com.au>
2015-02-08 15:55:19 +1100

    implemented group#users & group#videos with tests

--- lib/vimeo/entities/group.rb
+++ lib/vimeo/entities/group.rb
@@ -1,8 +1,29 @@
 module Vimeo
   module Entities
     class Group < Vimeo::Base
-      def users; end
-      def videos; end
+      include Vimeo::Helpers
+
+      ##
+      # Get a list of videos in a group
+      def videos options = {}
+        perform_get_with_object("/groups/#{get_id}/videos", options, Vimeo::Entities::Video)
+      end
+
+      ##
+      # Get a list of users that have joined a group
+      def users options = {}
+        perform_get_with_object("/groups/#{get_id}/users", options, Vimeo::Entities::User)
+      end
+
+      ##
+      # Create a new group
+      def create
+      end
+
+      ##
+      # Delete a group
+      def delete
+      end
     end
   end
 end
