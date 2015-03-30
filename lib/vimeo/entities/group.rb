38e2f18ff61414d2ed14aa69c111fe83156026fc
Colby Swandale <colby@taplaboratories.com.au>
2015-03-30 22:09:15 +1100

    group implementation

--- lib/vimeo/entities/group.rb
+++ lib/vimeo/entities/group.rb
@@ -3,6 +3,16 @@ module Vimeo
     class Group < Vimeo::Base
       include Vimeo::Helpers
 
+      class << self
+        ##
+        # Create a new group
+        def create client, options
+          instance = new(client, options)
+          instance.perform_post("/channels", options)
+          instance
+        end
+      end
+
       ##
       # Get a list of videos in a group
       def videos options = {}
@@ -16,13 +26,23 @@ module Vimeo
       end
 
       ##
-      # Create a new group
-      def create
+      # Delete a group
+      def delete
+        !!perform_delete("/groups/#{get_id}")
       end
 
       ##
-      # Delete a group
-      def delete
+      # Check if a Group has a video.
+      def has_video? video_id
+        !!perform_get("/groups/#{get_id}/videos/#{video_id}")
+      end
+
+      def add_video video_id
+        !!perform_put("/groups/#{get_id}/videos/#{video_id}")
+      end
+
+      def remove_video video_id
+        !!perform_delete("/groups/#{get_id}/videos/#{video_id}")
       end
     end
   end
