8107bcebd1f20b347a930a6496e2a2cf8ca5bd1e
Colby Swandale <colby@taplaboratories.com.au>
2015-04-03 21:18:27 +1100

    tag videos

--- lib/vimeo/entities/tag.rb
+++ lib/vimeo/entities/tag.rb
@@ -1,6 +1,11 @@
 module Vimeo
   module Entities
     class Tag < Vimeo::Base
+      ##
+      # Get a list of videos associated with a tag.
+      def videos
+        perform_get_with_object("/tags/#{get_id}/videos", options, Vimeo::Entities::Video)
+      end
     end
   end
 end
