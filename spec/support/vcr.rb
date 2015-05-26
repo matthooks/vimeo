35ba133a7c7ff11f527907d2f6ee8c0a46f13f33
Colby Swandale <colby@taplaboratories.com.au>
2015-05-26 22:34:41 +1000

    revert vcr to use webmock

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,6 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
-  config.hook_into :faraday
+  config.hook_into :webmock
   config.filter_sensitive_data '<ACCESS_TOKEN>' do |interaction|
     interaction.request.headers['Authorization'].first
   end
