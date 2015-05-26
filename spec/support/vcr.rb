b7f9ed3b130b581e4cf17165a3f1a10213736e1b
Colby Swandale <colby@taplaboratories.com.au>
2015-05-26 21:48:51 +1000

    vcr hook_into faraday, change cassete naming

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,6 @@
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/cassettes"
-  config.hook_into :webmock
+  config.hook_into :faraday
   config.filter_sensitive_data '<ACCESS_TOKEN>' do |interaction|
     interaction.request.headers['Authorization'].first
   end
@@ -8,7 +8,7 @@ end
 
 RSpec.configure do |c|
   c.around(:each, :vcr) do |example|
-    name = example.metadata[:full_description]
+    name = [example.metadata[:full_description].strip, example.metadata[:line_number]].join('_')
     VCR.use_cassette(name) { example.call }
   end
 end
