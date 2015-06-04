048f2b283959f338eef9f388fd944f08b3fa664e
Colby Swandale <colby@taplaboratories.com.au>
2015-06-04 21:16:51 +1000

    tag documentation

--- lib/vimeo/endpoints/tags.rb
+++ lib/vimeo/endpoints/tags.rb
@@ -5,8 +5,10 @@ module Vimeo
 
       ##
       # Get a Tag
-      def tag word, options = {}
-        perform_get_with_object("/tags/#{word}", options, Vimeo::Entities::Tag)
+      #
+      # *word:* the tag's name
+      def tag word
+        perform_get_with_object("/tags/#{word}", {}, Vimeo::Entities::Tag)
       end
     end
   end
