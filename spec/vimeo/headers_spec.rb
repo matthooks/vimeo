9bc567c86db8c5988fe2871acd83bdafc420d1b4
Colby Swandale <colby@taplaboratories.com.au>
2015-06-09 23:02:53 +1000

    updated haders spec for correct auth convention, client uses $vimeo_access_token

--- spec/vimeo/headers_spec.rb
+++ spec/vimeo/headers_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 
 describe Vimeo::Headers do
   let(:client) do
-    Vimeo::Client.new {|c| c.access_token = 'hgf' }
+    Vimeo::Client.new {|c| c.access_token = $vimeo_access_token }
   end
 
   describe '#request_headers' do
     subject { Vimeo::Headers.new(client).request_headers }
+
     it { is_expected.to have_key :user_agent }
     it { is_expected.to have_key :authorization }
+
+    it 'has a bearer authentication token' do
+      expect(subject[:authorization]).to match /bearer \w*/
+    end
   end
 end
